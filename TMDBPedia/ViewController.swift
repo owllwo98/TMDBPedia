b9f47b53b0cfadd68146810b4d483cc4eb62fac4
owllwo98 <sloopy98@naver.com>
2025-01-24 12:44:17 +0900

    Initial Commit

---
+++ TMDBPedia/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TMDBPedia
+//
+//  Created by 변정훈 on 1/24/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
